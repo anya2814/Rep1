23689d8f06774662f5247ece0ad5645aeab413d9
anya2814 <anyaklimenko@inbox.ru>
2023-04-16 23:49:13 +0700

    File monitor descriptions and Qobject
    Добавление в класс FileMonitor наследования от QObject и немного описаний

--- FileTracking/filemonitor.h
+++ FileTracking/filemonitor.h
@@ -1,16 +1,21 @@
 #ifndef FILEMONITOR_H
 #define FILEMONITOR_H
 
+#include <Qobject>
 
-class FileMonitor
+class FileMonitor : public QObject
 {
+    Q_OBJECT
 public:
-    FileMonitor();
-    void AddFile(QString fileName);
-    void DelFile(QString fileName);
-    void CheckStatesFiles();
+    FileMonitor(QVector<FileState> objects = NULL);
+    void AddFile(QString fileName);     // добавление файла в группу файлов за которыми следим
+    void DelFile(QString fileName);     // удаление файла из группы
+public slots:
+    void CheckStatesFiles();    // проверка состояний файлов
+signals:
+    void FileChanged();
 private:
-    QVector<FileState> objects;
+    QVector<FileState> objects;     // вектор объектов класса FileState, в которых хранится путь к файлу и его состояние (существует, размер)
 };
 
 #endif // FILEMONITOR_H
